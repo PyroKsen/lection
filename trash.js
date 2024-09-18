ecbd7fb76bd9d70280ce76e9ed5530471621f1b8
PyroKsen <maxmax11219@yandex.ru>
2024-09-18 12:08:16 +0300

    er5jty

--- trash.js
+++ trash.js
@@ -3,10 +3,52 @@ class TribeMember {
         this.name = name
         this.age = Math.round(Math.random()*100)
         this.health = Math.round(Math.random()*100)
+        this.nakopitel = 0
+        this.tools = []
+        this.damage = 7
     }
+
     getInfo() {
         console.log(`aboba ${this.name}, возраст ${this.age}, хп ${this.health}`)
     }
+
+    takeDamage(damage) {
+        this.health -= damage
+        console.log(`${this.name} take ${damage} damage. health: ${this.health}`)
+        if (this.health <= 0) {
+            console.log(`${this.name} died because blood can't move outside body`)
+            return true
+        } else {
+            return false
+        }
+    }
+
+    attack(target) {
+        console.log(`\n${this.name} attack ${target.name}`)
+        if (this.tools.length === 0) {
+            console.log(`${this.name} base attack`)
+        } else {
+            console.log(`${this.name} attack ${this.tools.at(0).name}`)
+        }
+        let tool = this.tools.at(0) || 0
+        let toolDamage = tool.damage
+        console.log(tool)
+        if (tool.use()) {
+            let iskilled = target.takeDamage(this.damage + tool.damage)
+            if (iskilled) {
+                target = null
+            }
+        }
+    }
+
+    addTool(tool) {
+        this.tools.push(tool)
+    }
+
+    getToolList() {
+        const list = this.tools.map(({name, durability}) => `${name}, ${durability}`)
+        console.log(`${this.name} have: ${list.join('; ')}`)
+    } 
 }
 
 class Apache extends TribeMember {
@@ -16,28 +58,22 @@ class Apache extends TribeMember {
         if (this.health >= 40) {
             this.health = Math.round(Math.random()* 40)
         }
-        this.tools = []
     }
+
     getDescription() {
         this.getInfo()
         console.log(`aboba ${this.name} апатчанин с земледелием ${this.farmingSkill}`)
     }
-    addTool(tool) {
-        this.tools.push(tool)
-    }
-    getToolList() {
-        const list = this.tools.map(({name, durability}) => `${name}, ${durability}`)
-        console.log(`${this.name} have: ${list.join('; ')}`)
-    }
-    ultraUse() {
-        if (this.nakopitelForSuperUse === 3) {
-            this.damage += 2
-            console.log(`Super udar! damage: ${this.damage}`)
-            this.damage -= 2
-            this.nakopitelForSuperUse = 0
+
+    ultraUse(tool) {
+        if ((tool.durability > 0) && (this.nakopitel === 3)) {
+            tool.useDurability()
+            console.log(`damage: ${tool.damage + 2}, UP`)
+            this.nakopitel = 0
         } else {
-            console.log(`damge ${this.damage}`)
-            this.nakopitelForSuperUse += 1
+            tool.useDurability()
+            console.log(`${tool.name} - 1 dur, Now ${3 - this.nakopitel} for superPunch`)
+            this.nakopitel += 1
         }
     }
 }
@@ -50,6 +86,7 @@ class Rednek extends TribeMember {
             this.health = Math.round(Math.random()* 40) + 60
         }
     }
+
     getDescription() {
         this.getInfo()
         console.log(`aboba ${this.name} реднек с войной ${this.war}`)
@@ -61,6 +98,7 @@ class Item {
         this.name = name
         this.durability = Math.round(Math.random()* 5)
     }
+
     use() {
         if (this.durability > 0) {
             this.durability -= 1
@@ -83,18 +121,26 @@ class Weapon extends Item {
 class Tools extends Item {
     constructor(name) {
         super(name)
-        this.nakopitelForSuperUse = 0
         this.damage = 2
+    }
+
+    useDurability() {
+        this.durability -= 1
     }
 }
 
 const Vitaly = new Apache('Vitaly')
 Vitaly.getDescription()
-const Vadim = new Rednek('Vadim')
-Vadim.getDescription()
+const Daniil = new Rednek('Daniil')
+Daniil.getDescription()
 
 const motiga = new Tools('motiga')
-const shovel = new Tools('shovel')
 Vitaly.addTool(motiga)
-Vitaly.addTool(shovel)
-Vitaly.getToolList()
+
+const axe = new Weapon('axe')
+Daniil.addTool(axe)
+Daniil.getToolList()
+
+Vitaly.attack(Daniil)
+Vitaly.attack(Daniil)
+Vitaly.attack(Daniil)
